962bcf0cb1c55e0635fd89a5bbfbfa2b7c530a6a
destsummer <46880204+destsummer@users.noreply.github.com>
2020-11-06 14:37:31 -0500

    Delete step3.sh

--- step3.sh
+++
@@ -1,5 +0,0 @@
-python organize.py /home/stamang/clever-sinai/clever-snomed/output/ants /home/stamang/clever-sinai/clever-snomed/res/dicts/snomed4clever.txt /home/stamang/clever-sinai/clever-snomed/res/corpus/mimic3/testnotemdata.txt /home/stamang/clever-sinai/clever-snomed/output/linkedAnts.txt
-
-#ppath = sys.argv[1]      #project path where the output of step2 lives                                                                               
-#dictfile = sys.argv[2]   #terminology file                                                                                                            
-#noteMdata = sys.argv[3]  #note metadata                                                                                                               #ptAntfile = sys.argv[4]  #annotation file
